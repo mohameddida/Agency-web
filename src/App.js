f783188af7ba44c70b823d075b67b53c2251a068
mohameddida <mohameddida891@gmail.com>
2024-09-23 01:13:41 +0100

    project are ready to deploy v2

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ const App = () => {
         <section id="home" className="relative">
           <div className="bg-custom-image bg-cover bg-center h-screen flex items-center">
             <div className="absolute inset-0 bg-gradient-to-b from-dark-blue to-transparent"></div>
-            <div className="container absolute w-96 mx-auto text-left mx-20">
-              <p className="text-4xl md:text-5xl lg:text-6xl font-bold text-white text-shadow">
+            <div className="container absolute w-96  text-left mx-20">
+              <p className="text-4xl md:text-5xl lg:text-6xl mx-12 font-bold text-white text-shadow">
                 Find Your <span className="text-yellow-300">Best</span> Tours
                 with <span className="text-yellow-300">Us</span>
               </p>
