3fa7ed7892d40bc8645183ffcab58810ef72ca4a
mohameddida <mohameddida891@gmail.com>
2024-09-23 11:23:16 +0100

    test deploy v6

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1600px",
+        "4xl": "1800px",
+        custom: "2000px",
+      },
       backgroundImage: {
         "custom-image": "url('/src/assets/homeCover.png')",
       },
